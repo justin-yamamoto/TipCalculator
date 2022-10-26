ea8f71d60048e42611fb83da48473807880b7237
justin-yamamoto <justinyamamoto2@gmail.com>
2022-10-26 16:09:35 -0700

    2nd commit

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ tip20_amount = float(new_bill_amount * .20)
 tip18_amount = float(new_bill_amount * .18)
 tip15_amount = float(new_bill_amount * .15)
 
-print("Tip Amounts: \n")
-print("20%: " + "$" + str(tip20_amount))
-print("18% " + "$" + str(tip18_amount))
-print("15%" + "$" + str(tip15_amount))
+total_people = input("How many people to split the bill? \n")
+new_total_people = int(total_people)
+
+print("Tip Amount Per Person: \n")
+print("20%: " + "$" + str(tip20_amount/new_total_people))
+print("18% " + "$" + str(tip18_amount/new_total_people))
+print("15% " + "$" + str(tip15_amount/new_total_people))
 
